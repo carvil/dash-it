ebd9b432607ff43d74c7a20722090ec319b77605
Carlos Vilhena <carlosvilhena@gmail.com>
2012-02-13 21:57:15 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DashIt::Application.config.secret_token = 'fb0767459d09807918379013abed49c12d1426988d0e78d947227c9239fe779f01f23764df9d6e22470fb4dd84946fd3241f89bcac59b81a4ca6c1dbd097150d'
