c9d974fa337d8bfa3e2c61bcf8ed6bd42b5a6176
Carlos Vilhena <carlosvilhena@gmail.com>
2012-02-14 15:29:54 +0000

    added specs to project model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "given a valid set of attributes" do
+    it "should save the project" do
+      project = FactoryGirl.build(:project)
+      project.valid?.should be_true
+      project.save.should be_true
+    end
+  end
+
+  context "given invalid attributes" do
+    it "should not save the project" do
+      project = Project.new
+      project.valid?.should be_false
+      project.save.should be_false
+    end
+  end
 end
