ee1fc1f45085b9ae092c7e2f2bd5ac4ba69caea8
Carlos Vilhena <carlosvilhena@gmail.com>
2012-03-17 22:31:06 +0000

    check if user is nil

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
 
-    if @user.persisted?
+    if @user and @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
       sign_in_and_redirect @user, :event => :authentication
     else
