86dba5a01146c0bdaee318b11ce5639946ca58d7
Carlos Vilhena <carlosvilhena@gmail.com>
2012-03-17 23:41:10 +0000

    refactor omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,27 +2,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
     @user = User.find_for_omniauth_oauth(request.env["omniauth.auth"], current_user)
-    if @user
-      persist_user @user, "Github", "devise.github_data"
-    else
-      flash[:error] = I18n.t "devise.omniauth_callbacks.noemail", :kind => "Github"
-      redirect_to new_user_registration_url
-    end
+    persist_user @user, "Github", "devise.github_data"
   end
 
-  def twitter
+  def facebook
     @user = User.find_for_omniauth_oauth(request.env["omniauth.auth"], current_user)
-    persist_user @user, "Twitter", "devise.twitter_data"
+    persist_user @user, "Facebook", "devise.facebook_data"
   end
 
   private 
 
   def persist_user user, kind, session_name
-    if user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
-      sign_in_and_redirect user, :event => :authentication
+    if user
+      if user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: kind
+        sign_in_and_redirect user, :event => :authentication
+      else
+        session[session_name] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     else
-      session[session_name] = request.env["omniauth.auth"]
+      flash[:error] = I18n.t "devise.omniauth_callbacks.noemail", kind: kind
       redirect_to new_user_registration_url
     end
   end
