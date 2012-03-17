e797bc66d6d91a9ef86202fa6d6047e0d008636c
Carlos Vilhena <carlosvilhena@gmail.com>
2012-03-17 22:36:22 +0000

    throw error if user is nil (no email address in public profile)

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,12 +2,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
 
-    if @user and @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
-      sign_in_and_redirect @user, :event => :authentication
+    if @user
+      if @user.persisted?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Github"
+        sign_in_and_redirect @user, :event => :authentication
+      else
+        session["devise.github_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     else
-      session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      flash[:error] = I18n.t "devise.omniauth_callbacks.noemail", :kind => "Github"
     end
   end
 end
