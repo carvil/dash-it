8cf1a1a91fe309258fdf0a60a138b8718b70d854
Carlos Vilhena <carlosvilhena@gmail.com>
2012-03-17 18:54:21 +0000

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120222171309) do
+ActiveRecord::Schema.define(:version => 20120317185234) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(:version => 20120222171309) do
     t.integer  "project_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
