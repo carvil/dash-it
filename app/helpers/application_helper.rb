cff78a6e10e26705f55979089156296ed998de57
Carlos Vilhena <carlosvilhena@gmail.com>
2012-03-18 15:00:35 +0000

    add helper methods to show error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def build_message_div(key, messages = [])
+    unless messages.empty?
+      content_tag(:div, :class => "alert alert-block alert-#{key} fade in") do
+        concat link_to "x", "#", {:class => "close", "data-dismiss" => "alert" }
+        concat content_tag :p, "Oh snap! You got errors. Please change and try again."
+        concat build_messages_list messages
+      end
+    end
+  end
+
+  def build_messages_list messages
+    content_tag :ul do
+      messages.collect {|message| concat(content_tag(:li, message.to_s)) }
+    end
+  end
+
+  def display_error_messages!
+    build_message_div "error", resource.errors.full_messages
+  end
 end
