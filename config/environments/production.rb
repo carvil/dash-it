b43fe429779b8a228d042f8b77a0316ed50a4136
Carlos Vilhena <carlosvilhena@gmail.com>
2012-03-18 00:38:27 +0000

    fixing smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,12 +52,14 @@ DashIt::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'dashit.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-    ActionMailer::Base.smtp_settings = {
-    :address  => "smtp.gmail.com",
-    :port  => 465,
-    :user_name  => ENV["GMAIL_EMAIL"],
-    :password  => ENV["GMAIL_PASSWORD"],
-    :authentication  => :login
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'baci.lindsaar.net',
+    :user_name            => ENV["GMAIL_EMAIL"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
   }
 
   # Enable threaded mode
